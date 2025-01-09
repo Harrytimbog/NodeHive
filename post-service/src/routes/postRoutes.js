1835e825fc547081436c37374f427a50fbff2a01
timilehin arigbede <harriemannie@gmail.com>
2025-01-09 19:32:34 +0000

    Auth Middleware to Validate that Post Creator is authenticated | Post Service

--- post-service/src/routes/postRoutes.js
+++ post-service/src/routes/postRoutes.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
 const { createPost, getPosts, getAllPost, deletePost } = require('../controllers/postController');
+const authenticateRequest = require('../middleware/authMiddleware');
 
 // middleware -> this will tell if the user is authenticated or not
 
+
+// router.use(authenticateRequest);
+router.post('/create-post', authenticateRequest, createPost);
+
+module.exports = router;
