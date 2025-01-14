d6c727a49ddbf14e91c3545ddb9a37f55fd0c9a2
timilehin arigbede <harriemannie@gmail.com>
2025-01-14 18:57:54 +0000

    Add mediaId validation in for Post create action

--- post-service/src/utils/validation.js
+++ post-service/src/utils/validation.js
@@ -4,6 +4,7 @@ const validateCreatePost = (data) => {
   const schema = joi.object({
     title: joi.string().min(3).required(),
     content: joi.string().min(6).required(),
+    mediaIds: joi.array().items(joi.string()).required(),
   });
 
   return schema.validate(data);
